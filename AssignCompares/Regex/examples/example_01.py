9a186f35b355a6372d8d125488e06e21f79187ab
CollaQuentin <q.colla@student.uclouvain.be>
2025-06-01 03:36:09 +0200

    AssignCompares Examples

---
+++ AssignCompares/Regex/examples/example_01.py
@@ -0,0 +1,10 @@
+def sycaruse(s0):
+    nummer = s0
+    if nummer = 1 :
+        print(1)
+    elif nummer % 2 == 0:
+        print(nummer/2)
+        nummer= nummer/2
+    else:
+        print ((3*nummer)+1)
+        nummer= ((3*nummer)+1)
